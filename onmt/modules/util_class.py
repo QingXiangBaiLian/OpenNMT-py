32309863e02dc8b6569c8fd40dc94d834a0cba60
xuchao <xuchao@microsoft.com>
2022-08-01 09:51:52 +0800

    use Distance

--- onmt/modules/util_class.py
+++ onmt/modules/util_class.py
@@ -2,6 +2,8 @@
 import torch
 import torch.nn as nn
 
+from torch.autograd import Function
+
 
 # At the moment this class is only used by embeddings.Embeddings look-up tables
 class Elementwise(nn.ModuleList):
@@ -85,15 +87,63 @@ class PoincareReparametrize(nn.Module):
               ``(batch, 1)``.
         """
         #euclidean norm
-        sqvnorm = torch.sum(e * e, dim=-1)
+        # sqvnorm = torch.sum(e * e, dim=-1)
         res = []
-        for u_slice in u.split(1,0):
-            squnorm = torch.sum(u_slice * u_slice, dim=-1)
-            sqdist = torch.sum(torch.pow(u_slice - e, 2), dim=-1)
-            #fraction
-            x = sqdist / ((1 - squnorm) * (1 - sqvnorm)) * 2 + 1
-            # arcosh
-            z = torch.sqrt(torch.pow(x, 2) - 1)
-            res.append(torch.log(x + z))
+        # for u_slice in u:
+        #     d = Distance.apply(u_slice, e)
+        #     res.append(d)
+ 
+        # for e_slice in e:
+        #     d = Distance.apply(u, e_slice)
+        #     res.append(d)
+        # return torch.stack(res, 1)
+        for u_slice in u:
+            # squnorm = torch.sum(u_slice * u_slice, dim=-1)
+            # sqdist = torch.sum(torch.pow(u_slice - e, 2), dim=-1)
+            # #fraction
+            # x = sqdist / ((1 - squnorm) * (1 - sqvnorm)) * 2 + 1
+            # # arcosh
+            # z = torch.sqrt(torch.pow(x, 2) - 1)
+            # d = torch.log(x + z)
+            d = []
+            for e_slice in e:
+                d0 = Distance.apply(u_slice, e_slice)
+                d.append(d0)
+            d = torch.stack(d, 0)
+            res.append(d)
 
         return torch.stack(res,0)
+
+
+class Distance(Function):
+    @staticmethod
+    def grad(x, v, sqnormx, sqnormv, sqdist, eps = 1e-5):
+        alpha = (1 - sqnormx)
+        beta = (1 - sqnormv)
+        z = 1 + 2 * sqdist / (alpha * beta)
+        a = ((sqnormv - 2 * torch.sum(x * v, dim=-1) + 1) / torch.pow(alpha, 2))\
+            .unsqueeze(-1).expand_as(x)
+        a = a * x - v / alpha.unsqueeze(-1).expand_as(v)
+        z = torch.sqrt(torch.pow(z, 2) - 1)
+        z = torch.clamp(z * beta, min=eps).unsqueeze(-1)
+        return 4 * a / z.expand_as(x)
+
+    @staticmethod
+    def forward(ctx, u, v, eps = 1e-5):
+        squnorm = torch.clamp(torch.sum(u * u, dim=-1), 0, 1 - eps)
+        sqvnorm = torch.clamp(torch.sum(v * v, dim=-1), 0, 1 - eps)
+        sqdist = torch.sum(torch.pow(u - v, 2), dim=-1)
+        ctx.eps = eps
+        ctx.save_for_backward(u, v, squnorm, sqvnorm, sqdist)
+        x = sqdist / ((1 - squnorm) * (1 - sqvnorm)) * 2 + 1
+        # arcosh
+        z = torch.sqrt(torch.pow(x, 2) - 1)
+        return torch.log(x + z)
+
+    @staticmethod
+    def backward(ctx, g):
+        u, v, squnorm, sqvnorm, sqdist = ctx.saved_tensors
+        g = g.unsqueeze(-1)
+        gu = Distance.grad(u, v, squnorm, sqvnorm, sqdist, ctx.eps)
+        gv = Distance.grad(v, u, sqvnorm, squnorm, sqdist, ctx.eps)
+        return g.expand_as(gu) * gu, g.expand_as(gv) * gv, None
